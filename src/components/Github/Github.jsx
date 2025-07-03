715024293c80aba3c3748574f6e5696bad3c2d39
Himanshu Singh <himanshusingh83683@gmail.com>
2025-07-03 17:12:24 +0530

    Inital commit: REact Routter practice project

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+function Github() {
+  const data = useLoaderData();
+  // const [data,setData] = useState([]);
+
+  // useEffect(()=>{
+  //   fetch('https://api.github.com/users/harshsinghpujari')
+  //   .then(res => 
+  //     res.json()
+  //   )
+  //   .then(data => 
+  //       setData(data)
+  //   )
+  // },[])
+
+  return (
+    <div className='text-center m-4 bg-gray-500 text-white p-4 text-3xl'>
+      Github Followers: {data.followers} 
+
+      <img className='rounded-full' src={data.avatar_url} alt=" git profile pic" width={300}/>
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async() => {
+  const response = await fetch("https://api.github.com/users/harshsinghpujari");
+  return response.json();
+
+}
